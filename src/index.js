5c4e4778863fe5f8ab2d9105c4c224beac3d5d89
antony-hospkins <106913820+antony-hospkins@users.noreply.github.com>
2022-06-08 00:27:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,65 +1,78 @@
+const fs = require('fs')
 const { Telegraf, Markup } = require('telegraf')
-const { messages } = require('./messages/messages')
+const LocalSession = require('telegraf-session-local')
 require('dotenv').config()
+const { messages } = require('./messages/messages')
+const writeAnswers = require('./writeAnswers')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-let current_step = 0
-let is_ready_to_work = null
-let contact_on_telegram = true
+
+bot.use((new LocalSession({ database: 'session.json' })).middleware())
 
 bot.start(async (ctx) => {
-	if (current_step !== -1) {
-		current_step = 0;
-		await ctx.reply(messages.start_step.message)
-		return current_step++
+	ctx.session.current_step = 'READY'
+	ctx.session.userData = {
+		name: `${ctx.message?.from?.first_name} ${ctx.message?.from?.last_name}`,
+		username: ctx.message?.from?.username,
+		id: ctx.message?.from?.id
 	}
+	await ctx.replyWithPhoto({ source: messages.start_step.photo })
+	return ctx.reply(messages.start_step.message)
 })
 
 bot.on('message', async (ctx) => {
-	console.log('current_step:', current_step);
-
-	if (current_step === 1) {
-		if (ctx.update?.message?.text.includes('+')) {
-			await ctx.reply(messages.first_step.message)
-			return current_step++
-		} else {
-			await ctx.reply('Извини, я не понимаю тебя. Пожалуйста, следуй сообщению в инструкции')	
+	if (ctx.update?.message?.text !== undefined) {
+		if (ctx.session.current_step === 'READY') {
+			if (ctx.update?.message?.text.includes('+')) {
+				ctx.session.current_step = 'AGE'
+				await ctx.replyWithPhoto({ source: messages.age.photo })
+				return ctx.reply(messages.age.message)
+			} else {
+				return ctx.reply('Извини, я не понимаю тебя. Пожалуйста, следуй сообщению в инструкции')
+			}
 		}
-	}
 	
-	if (current_step === 2) {
-		if (
-			Number(ctx.update?.message?.text)
-			|| ctx.update?.message?.text.includes('лет')
-			|| ctx.update?.message?.text.includes('год')
-		) {
-			await ctx.reply(messages.second_step.message)
-			return current_step++
-		} else {
-			await ctx.reply('Пожалуйста, укажи свой возраст цифрой')
+		if (ctx.session.current_step === 'AGE') {
+			if (Number(ctx.update?.message?.text) || ctx.update?.message?.text.includes('лет') || ctx.update?.message?.text.includes('год')) {
+				ctx.session.userData.age = ctx.update?.message?.text
+				ctx.session.current_step = 'DREAM_COUNTRY'
+				await ctx.replyWithPhoto({ source: messages.dreamCountry.photo })
+				return ctx.reply(messages.dreamCountry.message)
+			} else {
+				return ctx.reply('Пожалуйста, укажи свой возраст цифрой')
+			}
 		}
-	}
 	
-	if (current_step === 3) {
-		await ctx.reply(messages.third_step.message)
-		return current_step++
-	}
+		if (ctx.session.current_step === 'DREAM_COUNTRY') {
+			ctx.session.userData.dreamCountry = ctx.update?.message?.text
+			ctx.session.current_step = 'OWN_COUNTRY'
+			return ctx.reply(messages.country.message)
+		}
 	
-	if (current_step === 4) {
-		await ctx.reply(messages.fourth_step.message, Markup.inlineKeyboard(
-			messages.fourth_step.buttons.map((button, index) => {
-				return [Markup.button.callback(button, `desired-earnings-button-${index}`)]
-			})
-		))
-		return current_step++
-	}
+		if (ctx.session.current_step === 'OWN_COUNTRY') {
+			ctx.session.userData.ownCountry = ctx.update?.message?.text
+			ctx.session.current_step = 'DREAM_MONEY'
+			await ctx.replyWithPhoto({ source: messages.dreamMoney.photo })
+			return ctx.reply(messages.dreamMoney.message, Markup.inlineKeyboard(
+				messages.dreamMoney.buttons.map((button, index) => {
+					return [Markup.button.callback(button, `desired-earnings-button-${index}`)]
+				})
+			))
+		}
 	
-	// buttons ...
-
-	if (current_step === 9) {
-		await ctx.reply(messages.ninth_step.message)
-		
-		return current_step = -1
+		if (ctx.session.current_step === 'OTHER_CONTACT') {
+			ctx.session.current_step = 'FINAL'
+			ctx.session.userData.other_contact = ctx.update?.message?.text
+			await ctx.replyWithPhoto({ source: messages.final.photo })
+			await ctx.reply(messages.final.message)
+			return writeAnswers.googleSheets(ctx.session.userData)
+		}
+	
+		if (ctx.session.current_step === 'FINAL') {
+			// The end
+		}
+	} else {
+		return ctx.reply('Неверный формат ответа')
 	}
 })
 
@@ -67,63 +80,59 @@ const onClickButton = (id) => {
 	bot.action(id, async (ctx) => {
 		try {
 			await ctx.answerCbQuery()
-						
-			// Next step
-			if (current_step === 5) {
-				// Save choosed value to DB
-				console.log('Choosed desired earnings value:', id);
 
-				await ctx.reply(messages.fifth_step.message, Markup.inlineKeyboard(
-					messages.fifth_step.buttons.map((button, index) => {
+			if (ctx.session.current_step === 'DREAM_MONEY') {
+				ctx.session.userData.dreamMoney = messages.dreamMoney.buttons[id.replace(/desired-earnings-button-/, '')]
+				ctx.session.current_step = 'CONTENT'
+				return ctx.reply(messages.content.message, Markup.inlineKeyboard(
+					messages.content.buttons.map((button, index) => {
 						return [Markup.button.callback(button, `content-button-${index}`)]
 					})
 				))
-
-				return current_step++
 			}
 
-			if (current_step === 6) {
-				// Save choosed value to DB
-				console.log('Choosed content value:', id);
-	
-				await ctx.reply(messages.sixth_step.message, Markup.inlineKeyboard(
-					messages.sixth_step.buttons.map((button, index) => {
+			if (ctx.session.current_step === 'CONTENT') {
+				ctx.session.userData.content = messages.content.buttons[id.replace(/content-button-/, '')]
+				ctx.session.current_step = 'ABOUT'
+				await ctx.replyWithPhoto({ source: messages.about.photo })
+				return ctx.reply(messages.about.message, Markup.inlineKeyboard(
+					messages.about.buttons.map((button, index) => {
 						return [Markup.button.callback(button, `know-details-button-${index}`)]
 					})
 				))
-				return current_step++
 			}
 
-			if (current_step === 7) {
-				// Save choosed value to DB
-				console.log('Know details value:', id);
-
+			if (ctx.session.current_step === 'ABOUT') {
 				const is_model_ready = id === 'know-details-button-0'
-
+				ctx.session.userData.is_model_ready = is_model_ready
 				if (is_model_ready) {
-					await ctx.reply(messages.seventh_step.message, Markup.inlineKeyboard(
-						messages.seventh_step.buttons.map((button, index) => {
+					ctx.session.current_step = 'CONTACT_ON_TELEGRAM'
+					return ctx.reply(messages.contact_on_telegram.message, Markup.inlineKeyboard(
+						messages.contact_on_telegram.buttons.map((button, index) => {
 							return [Markup.button.callback(button, `contact-button-${index}`)]
 						})
 					))
-					return current_step++
 				} else {
-					await ctx.reply('пока + блок бот')
-					return current_step = -1
+					ctx.session.current_step = 1
+					return ctx.replyWithHTML(messages.future.message)
 				}
 			}
 
-			if (current_step === 8) {
-				contact_on_telegram = id === 'contact-button-0'
+			if (ctx.session.current_step === 'CONTACT_ON_TELEGRAM') {
+				const contact_on_telegram = id === 'contact-button-0'
+				ctx.session.userData.contact_on_telegram = contact_on_telegram
 
 				if (contact_on_telegram) {
-					await ctx.reply(messages.ninth_step.message)
-					return current_step++
+					ctx.session.current_step = 'FINAL'
+					await ctx.replyWithPhoto({ source: messages.final.photo })
+					await writeAnswers.googleSheets(ctx.session.userData)
+					return ctx.reply(messages.final.message)
 				} else {
-					await ctx.reply(messages.eighth_step.message)
-					return current_step++
+					ctx.session.current_step = 'OTHER_CONTACT'
+					return ctx.reply(messages.other_contact.message)
 				}
 			}
+
 		} catch (error) {
 			// ...
 		}
@@ -131,22 +140,22 @@ const onClickButton = (id) => {
 }
 
 // Handling the buttons
-messages.fourth_step.buttons.forEach((button, index) => {
+messages.dreamMoney.buttons.forEach((button, index) => {
 	onClickButton(`desired-earnings-button-${index}`)
 })
 
 // Handling the buttons
-messages.fifth_step.buttons.forEach((button, index) => {
+messages.content.buttons.forEach((button, index) => {
 	onClickButton(`content-button-${index}`)
 })
 
 // Handling the buttons
-messages.sixth_step.buttons.forEach((button, index) => {
+messages.about.buttons.forEach((button, index) => {
 	onClickButton(`know-details-button-${index}`)
 })
 
 // Handling the buttons
-messages.seventh_step.buttons.forEach((button, index) => {
+messages.contact_on_telegram.buttons.forEach((button, index) => {
 	onClickButton(`contact-button-${index}`)
 })
 
