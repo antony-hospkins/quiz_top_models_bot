614efe35099b0576f135798f5cd0b2075ef5eb25
antony-hospkins <106913820+antony-hospkins@users.noreply.github.com>
2022-06-11 15:58:29 +0300

    Create testGoogleSheets.js

---
+++ tests/testGoogleSheets.js
@@ -0,0 +1,18 @@
+const writeAnswers = require('../src/writeAnswers');
+require('dotenv').config();
+
+const testDataToTable = async () => {
+    const result = await writeAnswers.googleSheets({
+        age: '63',
+        dreamCountry: 'Молдова',
+        own_country: 'Великобритания',
+        dreamMoney: '2️⃣ 2000-2500$',
+        content: '1️⃣ Уровня Инстаграм / Тик-ток / Соц.сети 📷',
+        is_model_ready: true,
+        contact_on_telegram: false,
+        other_contact: 'Скайп: 777'
+    })
+    console.log(result)
+}
+
+module.exports = testDataToTable
